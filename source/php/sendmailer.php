15448c92b7aaf14154a9698cc8eaef001d7cbb76
ivserga <ivsa79@mail.ru>
2019-03-05 17:23:10 +0800

    phpmailerAdd

--- source/php/sendmailer.php
+++ source/php/sendmailer.php
@@ -1,23 +1,29 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
     // Файлы phpmailer
-    require ‘phpmailer.php’;
-    require ‘smtp.php’;
+    require '/PHPMailer/src/Exception.php';
+    require '/PHPMailer/src/PHPMailer.php';
+    require '/PHPMailer/src/SMTP.php';
+    
+   
     // Переменные
-    $name = $_POST[‘name’];
-    $number = $_POST[‘number’];
-    $email = $_POST[‘email’];
+    $name = $_POST['name'];
+    $number = $_POST['number'];
+    $email = $_POST['email'];
     // Настройки
     $mail = new PHPMailer;
     $mail->isSMTP(); 
-    $mail->Host = ‘smtp.yandex.ru’; 
+    $mail->Host = 'smtp.yandex.ru'; 
     $mail->SMTPAuth = true; 
-    $mail->Username = ‘sptyi’; // Ваш логин в Яндексе. Именно логин, без @yandex.ru
-    $mail->Password = ‘banzay’; // Ваш пароль
-    $mail->SMTPSecure = ‘ssl’; 
+    $mail->Username = 'sptyi@yandex.ru'; // Ваш логин в Яндексе. Именно логин, без @yandex.ru
+    $mail->Password = 'banzay'; // Ваш пароль
+    $mail->SMTPSecure = 'ssl'; 
     $mail->Port = 465;
-    $mail->setFrom(‘sptyi@yandex.ru’); // Ваш Email
-    $mail->addAddress(‘218808@mail.ru’); // Email получателя
-    $mail->addAddress(‘ivsa79@mail.ru’); // Еще один email, если нужно.
+    $mail->setFrom('sptyi@yandex.ru'); // Ваш Email
+    $mail->addAddress('218808@mail.ru'); // Email получателя
+    $mail->addAddress('ivsa79@mail.ru'); // Еще один email, если нужно.
+    $mail->CharSet = "UTF-8";
     /* Прикрепление файлов
     for ($ct = 0; $ct < count($_FILES[‘userfile’][‘tmp_name’]); $ct++) {
     $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES[‘userfile’][‘name’][$ct]));
@@ -31,13 +37,13 @@
     
     // Письмо
     $mail->isHTML(true); 
-    $mail->Subject = “Заявка с сайта”; // Заголовок письма
-    $mail->Body = “Имя $name . Телефон $number . Почта $email”; // Текст письма
+    $mail->Subject = "Заявка с сайта"; // Заголовок письма
+    $mail->Body = "Имя $name . Телефон $number . Почта $email"; // Текст письма
     // Результат
     if(!$mail->send()) {
-    echo ‘Message could not be sent.’;
-    echo ‘Mailer Error: ‘ . $mail->ErrorInfo;
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
     } else {
-    echo ‘ok’;
+    echo 'ok';
     }
 ?>
